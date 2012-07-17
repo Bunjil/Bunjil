cbbbbd0ffcf03e222b226752c6e41ad5eff043c0
KarlGlaser <6955002@student.swin.edu.au>
2012-07-17 17:54:10 +0000

    KG - 32782787
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@124 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/app/jobs/landsat_rss_reader_job.rb
+++ bunjil/app/jobs/landsat_rss_reader_job.rb
@@ -1,20 +1,46 @@
 require 'feedzirra'
+require 'nokogiri'
 
 class LandsatRssReaderJob
 
   def perform
-    feed     = Feed.find_by_name("LandSat7")
+    feed = Feed.find_by_name("LandSat7")
     rss_data = Feedzirra::Feed.fetch_and_parse(feed.url)
-
-    rss_data.entries.each do |item|
-      unless FeedItem.find_by_title item.title
-        feed_item         = FeedItem.new
-        feed_item.title   = item.title
-        feed_item.feed_id = feed.id
-        feed_item.save
+    unless rss_data.is_a?(Fixnum)
+    # work with the feeds object
+      rss_data.entries.each do |item|
+        unless FeedItem.find_by_title item.title
+          # Only new entries: ignore entries with duplicate title.
+          handle_new_item item
+        end
       end
     end
+  end
 
+  # This runs for each new feed.
+  def handle_new_item item
+    feed_item         = FeedItem.new
+    feed_item.title   = item.title
+    feed_item.feed_id = feed.id
+    feed_item.link   = item.link
+    
+    feed_item.save
+    # Parse for points and url.
+
+    # Create area update with url and feed lat/long points.
+    au=AreaUpdate.new
+    au.init feed_item
+    au.image_url=format_image_url item.description
+    puts au.image_url
+    if au.should_update?
+      au.save
+      au.create_download_job # uses the image downloader
+    end
+  end
+
+  def format_image_url raw
+    doc=Nokogiri::HTML(raw)
+    doc.xpath('//a[1]/@href').text
   end
 
 end
