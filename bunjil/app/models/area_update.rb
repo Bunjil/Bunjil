dcbfee58aa9a0cd80fa5af266f2360f3e5c99669
KarlGlaser <6955002@student.swin.edu.au>
2012-07-17 17:54:58 +0000

    KG - 32723847
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@125 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/app/models/area_update.rb
+++ bunjil/app/models/area_update.rb
@@ -1,3 +1,42 @@
+require 'open-uri'
+require 'nokogiri'
+
 class AreaUpdate < ActiveRecord::Base
+	# new columns need to be added here to be writable through mass assignment
+
+  # sets all except image url because it's not on the details page.
+  # must call save after
+  def init(item)
+  	doc=Nokogiri::HTML(open(item.get_formatted_url))
+  	self.tl_lat=search doc, 'Corner Upper Left Latitude'
+  	self.tr_lat=search doc, 'Corner Upper Right Latitude'
+  	self.br_lat=search doc, 'Corner Lower Right Latitude'
+  	self.bl_lat=search doc, 'Corner Lower Left Latitude'
+  	self.tl_lon=search doc, 'Corner Upper Left Longitude'
+  	self.tr_lon=search doc, 'Corner Upper Right Longitude'
+  	self.br_lon=search doc, 'Corner Lower Right Longitude'
+  	self.bl_lon=search doc, 'Corner Lower Left Longitude'
+  	self.bl_lon=search doc, 'Corner Lower Left Longitude'
+  	self.cloud_cover=search doc, 'Cloud Cover'
+  	self.feed_item_id=item.feed_id
+  end
+
+  def search(doc,search)
+  	basePath = '/html/body/div/table/tbody/tr/td/a[text() ="'
+  	doc.xpath(basePath+search+'"]/../../td[2]').text
+  end
+
+  # Creates an intersection and returns true if this area meets 
+    # cloud cover requirements and intersects with an Area. 
+  def should_download?
+
+  end
+
+  # This method creeate image in the queue with image url that
+  	# must be already set
+  def create_download_job
+  	# TODO Aaron
+  end
+
 	has_one :area_update_download_task
 end
