8c7fa30d8cfba9bd80b191dc0ad254676b18fd0f
KarlGlaser <6955002@student.swin.edu.au>
2012-07-17 21:42:40 +0000

    KG - 29747233 - Created area model tests
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@127 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/test/unit/area_update_test.rb
+++ bunjil/test/unit/area_update_test.rb
@@ -1,11 +1,21 @@
 require 'test_helper'
 
 class AreaUpdateTest < ActiveSupport::TestCase
-  test "set" do
+  test "init from feed" do
   	item = feed_items(:one)
   	#item.get_formatted_url
   	ar = AreaUpdate.new
   	ar.init item
-  	assert ar.tl_lat
+
+    # if ar.should_update?
+    #   ar.save
+    #   ar.create_download_job # uses the image downloader
+    # end
+    assert ar.should_update?, "marked to be updated"
+  	#assert ar.tl_lat, "has a value for top left"
+  end
+
+  test "no collision" do
+  	points = Hash[:y, 32.22544, :h, 1.899, :x, -117.5592, :w, 2.454]
   end
 end
