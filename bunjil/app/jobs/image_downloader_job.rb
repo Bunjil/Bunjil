f0b3f9b348bcedb05e436d00baf641e42f8976c6
AaronLumaksana <6955215@student.swin.edu.au>
2012-07-19 06:14:18 +0000

    AL - 33030685 - Added logging to file rather than outputting to console for downloading
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@133 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/app/jobs/image_downloader_job.rb
+++ bunjil/app/jobs/image_downloader_job.rb
@@ -5,17 +5,37 @@ class ImageDownloaderJob
   def perform
     download_task = AreaUpdateDownloadTask.first()
     # checks to see if there are any download task and to skip if there aren't any.
-    while ( not download_task.nil?) 
+    while ( !download_task.nil?)
       # to get the name of the image from the url.
       image_name = download_task.image_url.split('/').last
-      puts "downloading " << image_name
+
+      log_downloading_image(image_name, download_task.image_url)
+
       open(image_name, 'wb') do |file|
         file << open(download_task.image_url).read
       end
-    # Delete task once image is downloaded
-    AreaUpdateDownloadTask.destroy(download_task)
-    # move on to next task (nil if none).
-    download_task = AreaUpdateDownloadTask.first()
+
+      log_download_success(image_name, download_task.image_url)
+      # Delete task once image is downloaded
+      AreaUpdateDownloadTask.destroy(download_task)
+      # move on to next task (nil if none).
+      download_task = AreaUpdateDownloadTask.first()
     end
   end
+
+  def image_logger
+    @@image_logger ||= Logger.new("#{Rails.root}/log/ImageDownloader.log")
+  end
+
+  def log_downloading_image(name, url)
+    image_logger.info("Downloading #{name} from #{url}")
+  end
+  def log_download_success(name, url)
+    image_logger.info("Downloaded #{name} from #{url}")
+  end
+  def log_download_fail(name, url)
+    image_logger.info("Failed to download #{name} from #{url}")
+  end
+
+
 end
