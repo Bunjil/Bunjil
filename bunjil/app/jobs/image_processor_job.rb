623d6815a9c964a9295d982a5bc0d69c6b702b36
DavidDesouza <6955312@student.swin.edu.au>
2012-07-31 09:58:09 +0000

    DD - 33570855 - Updated Image Processor to actually work. Have tested. One out standing issue is using 8bit depth vs 16 bit depth. This involves configuration of ImageMagick which i havent gotten configured properly to 8 bit.
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@141 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/app/jobs/image_processor_job.rb
+++ bunjil/app/jobs/image_processor_job.rb
@@ -4,51 +4,80 @@ include Magick
 class ImageProcessorJob
 
   def perform
-    lRedImage = ImageList.new("brazil1-3.tif")
-    lNIRImage = ImageList.new("brazil1-4.tif")
-    lNDVIImage = ImageList.new(lRedImage.columns, lRedImage.rows)
+    lRedPath = "/home/megurineluka/BunjilForestWatch/Codebase/bunjil/app/jobs/brazil1-3.tif"
+    lNIRPath = "/home/megurineluka/BunjilForestWatch/Codebase/bunjil/app/jobs/brazil1-4.tif"
+    lDestination = "/home/megurineluka/BunjilForestWatch/Codebase/bunjil/app/jobs/ndvi.jpg"
 
-    for width in 0..lRedImage.columns
-      for height in 0..lRedImage.rows
-        lRedPixelRed = lRedImage.pixel_color(width, height).red
-        lNIRPixelRed = lNIRImage.pixel_color(width, height).red
+    lRedImage = ImageList.new(lRedPath)
+    lNIRImage = ImageList.new(lNIRPath)
+    lNDVIImage = Image.new(lRedImage.columns, lRedImage.rows)
+
+    for x in 0..lRedImage.columns
+      for y in 0..lRedImage.rows
+        lRedPixel = lRedImage.pixel_color(x, y).red
+        lNIRPixel = lNIRImage.pixel_color(x, y).red
 
         lNDVI = calculate_ndvi(lRedPixel, lNIRPixel)
-        lNDVIPixel = color_result(lNDVIPixel)
 
-        lNDVIImage.pixel_color(width, height, lNDVIPixel);
+        lNDVIPixel = color_result(lNDVI)
+        #puts lRedPixel
+        #Remove this if ImageMagick configured to 8bit
+        lNDVIPixel = Pixel.new(lNDVIPixel.red * 257, lNDVIPixel.green * 257, lNDVIPixel.blue * 257)
+        lNDVIImage.pixel_color(x, y, lNDVIPixel)
 
       end
     end
 
-  	lNDVIImage.save
+  	lNDVIImage.write(lDestination)
   end
 
   #Returns the NDVI to Color map result for each pixel's NDVI
-  def color_result(double aValue)
-    if (aValue <= -0.2d) return Pixel.new(0,0,0,255)
-    if (aValue <= -0.1d) return Pixel.new(255, 0, 0, 255)
-    if (aValue <= 0) return Pixel.new(201, 0, 0, 255)
-    if (aValue <= 0.1d) return Pixel.new(136, 0, 0, 255)
-    if (aValue <= 0.2d) return Pixel.new(255, 255, 0, 255)
-    if (aValue <= 0.3d) return Pixel.new(185, 185, 0, 255)
-    if (aValue <= 0.4d) return Pixel.new(108, 108, 0, 255)
-    if (aValue <= 0.5d) return Pixel.new(0, 255, 255, 255)
-    if (aValue <= 0.6d) return Pixel.new(0, 128, 192, 255)
-    if (aValue <= 0.7d) return Pixel.new(0, 0, 255, 255)
-    if (aValue <= 0.8d) return Pixel.new(0, 255, 0, 255)
-    if (aValue <= 0.9d) return Pixel.new(0, 255, 64, 255)
-    if (aValue <= 1d) return Pixel.new(0, 128, 0, 255)
-    return Pixel.new(255, 255, 255, 255)
+  def color_result(aValue)
+    if aValue <= -0.2 
+      return Pixel.new(0,0,0,255)
+    elsif aValue <= -0.1 
+      return Pixel.new(255, 0, 0, 255)
+    elsif aValue <= 0 
+      return Pixel.new(201, 0, 0, 255)
+    elsif aValue <= 0.1 
+      return Pixel.new(136, 0, 0, 255)
+    elsif aValue <= 0.2 
+      return Pixel.new(255, 255, 0, 255)
+    elsif aValue <= 0.3 
+      return Pixel.new(185, 185, 0, 255)
+    elsif aValue <= 0.4 
+      return Pixel.new(108, 108, 0, 255)
+    elsif aValue <= 0.5 
+      return Pixel.new(0, 255, 255, 255)
+    elsif aValue <= 0.6 
+      return Pixel.new(0, 128, 192, 255)
+    elsif aValue <= 0.7 
+      return Pixel.new(0, 0, 255, 255)
+    elsif aValue <= 0.8 
+      return Pixel.new(0, 255, 0, 255)
+    elsif aValue <= 0.9 
+      return Pixel.new(0, 255, 64, 255)
+    elsif aValue <= 1 
+      return Pixel.new(0, 128, 0, 255)
+    else 
+      return Pixel.new(255, 255, 255, 255)
+    end
   end
 
-  def calculate_ndvi(integer aNIR, integer aRed)
-    double lNumerator = aNIR - aRed
-    if (lNumerator == 0) return 0
+  def calculate_ndvi(aNIR, aRed)
+    lNumerator = aNIR - aRed
+    if lNumerator == 0
+      return 0
+    end
 
-    double lDemoninator = aNIR + aRed
-    if (lDemoninator == 0) return 0
+    lDemoninator = aNIR + aRed
+    if lDemoninator == 0 
+      return 0
+    end
 
-    return lNumerator / lDemoninator
+    return lNumerator.to_f / lDemoninator
   end
 end
+
+x = ImageProcessorJob.new()
+x.perform
