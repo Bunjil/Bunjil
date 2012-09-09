68ef2d862a35f105643e4c194ea8ae0039eceb62
AaronLumaksana <6955215@student.swin.edu.au>
2012-09-09 15:33:11 +0000

    AL - 35726607, 35719463, 35719457 - manage to log in to earth explorer using mechanize (gem) should be smooth sailing... now deletes tar.gz after extraction of band 3 and 4.. calls image processor
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@177 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/app/jobs/image_downloader_job.rb
+++ bunjil/app/jobs/image_downloader_job.rb
@@ -1,7 +1,10 @@
 require 'open-uri'
 require 'rubygems/package'
 require 'zlib'
-
+require 'fileutils'
+require 'net/https'
+require 'net/http'
+require 'mechanize'
 
 class ImageDownloaderJob
 
@@ -24,21 +27,18 @@ class ImageDownloaderJob
           #archive_name = download_task.area_update.area_update_id
 
           archive_name = download_task.area_update.id
-          archive_url = IMAGE_BASE_URL.sub("${IMAGE_ID}",archive_name)
           # log downloading image
-          log_downloading_image(archive_name, archive_url)
+          log_downloading_image(archive_name)
           archive_file = "#{ARCHIVE_PATH}#{archive_name}"
           begin
-            open(archive_file, 'wb') do |file|
-              file << open(archive_url).read
-            end
+            download_from_earth_explorer(archive_name)
             #log download success
-            log_download_success(archive_name, archive_url)
+            log_download_success(archive_name)
             # Delete task once image is downloaded
             AreaUpdateDownloadTask.destroy(download_task)
 
           rescue Exception => e
-            log_download_fail(archive_name, archive_url, e.message)
+            log_download_fail(archive_name, e.message)
 
             download_task.retries += 1
             download_task.save
@@ -50,6 +50,7 @@ class ImageDownloaderJob
           end
 
           extract_tiffs("#{IMAGE_PATH}",archive_file)
+          perform_ndvi(archive_name,download_task.area_update)
         }
       end
       threads.each { |aThread|  aThread.join }
@@ -62,28 +63,49 @@ class ImageDownloaderJob
   end
 
 
-  def extract_tiffs(image_path, image_id)
-    reader = Gem::Package::TarReader.new(Zlib::GzipReader.open(image_id))
+  def extract_tiffs(image_id)
+    archive_file = "#{ARCHIVE_PATH}#{image_id}"
+    reader = Gem::Package::TarReader.new(Zlib::GzipReader.open(archive_file))
     reader.each do |entry|
       if (entry.full_name == image_id+'_B3.TIF') or (entry.full_name == image_id+'_B4.TIF') 
-        puts entry.full_name
-        open("#{image_path}#{entry.full_name}", 'wb') do |file|
+        # puts entry.full_name
+        open("#{IMAGE_PATH}#{entry.full_name}", 'wb') do |file|
           file << entry.read
         end
       end 
-
-      # puts entry.read
-    end    
+    end   
+    FileUtils.rm(archive_file) 
   end
 
-  def log_downloading_image(name, url)
-    IMAGE_DOWNLOAD_LOG.info("Downloading #{name} from #{url}")
+
+
+  def download_from_earth_explorer(image_id)
+    agent = Mechanize.new
+    page = agent.get('https://earthexplorer.usgs.gov/login')
+    login_form = page.forms[0]
+    login_form.username = 'bunjil'
+    login_form.password = 'Batman2012'
+    agent.submit(login_form)
+    agent.pluggable_parser.default = Mechanize::Download
+    # agent.max_file_buffer = 100
+    # agent.get("http://earthexplorer.usgs.gov/download/3373/LE71290232012244EDC00/STANDARD//").save('image_id')
+    agent.get("http://earthexplorer.usgs.gov/download/3373/#{image_id}/STANDARD//").save(image_id)
+    # agent.progressbar{ agent.download("http://earthexplorer.usgs.gov/download/3373/#{image_id}/STANDARD//",image_id)}
+
   end
-  def log_download_success(name, url)
-    IMAGE_DOWNLOAD_LOG.info("Downloaded #{name} from #{url}")
+
+  def perform_ndvi(image_id, area_update)
+    ImageProcessorJob.new.perform(area_update,"#{IMAGE_PATH}#{image_id}_B3.TIF","#{IMAGE_PATH}#{image_id}_B4.TIF")
   end
-  def log_download_fail(name, url, message)
-    IMAGE_DOWNLOAD_LOG.error("Failed to download #{name} from #{url} \n\t #{message}")
+
+  def log_downloading_image(name)
+    IMAGE_DOWNLOAD_LOG.info("Downloading #{name} from Earth Explorer")
+  end
+  def log_download_success(name)
+    IMAGE_DOWNLOAD_LOG.info("Downloaded #{name} from  Earth Explorer")
+  end
+  def log_download_fail(name, message)
+    IMAGE_DOWNLOAD_LOG.error("Failed to download #{name} from Earth Explorer \n\t #{message}")
   end
 
 
