9d9cd8cf1fc9c1168808f26523a0c357cd26e672
DavidDesouza <6955312@student.swin.edu.au>
2012-09-20 15:06:59 +0000

    DD - 34989079, 36378727 - Updated Image Processor to Remove old Band3 and Band4 Images, use relative pathing to save the processed image, and fixed the code format to follow the proper convention.
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@197 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/app/jobs/image_processor_job.rb
+++ bunjil/app/jobs/image_processor_job.rb
@@ -3,118 +3,65 @@ include Magick
 
 class ImageProcessorJob
 
-  def perform(aAreaUpdate, aRedPath, aNIRPath)
-    lDestination = "ndvi.jpg"
+  #Takes a red image and near infrared image and performs NDVI Calculation
+  def perform(area_update, red_path, nir_path)
+    destination = red_path +  '_ndvi.tif'
 
-    puts "Loading Red Image"
-    lRedImage = ImageList.new(aRedPath)
-    puts "Red Image Loaded"
-    puts "Loading NIR Image"
-    lNIRImage = ImageList.new(aNIRPath)
-    puts "NIR Image "
-    lNDVIImage = Image.new(lRedImage.columns, lRedImage.rows)
-    puts "NIR Image Loaded"
+    red_image = ImageList.new(red_path)
+    nir_image = ImageList.new(nir_path)
+    ndvi_image = Image.new(red_image.columns, red_image.rows)
 
-    puts "Columns: " + lRedImage.columns.to_s
-    puts "Rows: " + lRedImage.rows.to_s
-    puts "Commencing NDVI"
-    puts Time.now
-    val = 0
+    for x in 0..red_image.columns
+      for y in 0..red_image.rows
+        red_pixel = red_image.pixel_color(x, y).red
+        nir_pixel = nir_image.pixel_color(x, y).red
 
-    for x in 0..lRedImage.columns
-      for y in 0..lRedImage.rows
-        lRedPixel = lRedImage.pixel_color(x, y).red
-        lNIRPixel = lNIRImage.pixel_color(x, y).red
+        ndvi = calculate_ndvi(red_pixel, nir_pixel)
 
-        lNDVI = calculate_ndvi(lRedPixel, lNIRPixel)
-
-        lNDVIPixel = color_result(lNDVI)
-        lNDVIImage.pixel_color(x, y, lNDVIPixel)
+        ndvi_pixel = color_result(ndvi)
+        ndvi_image.pixel_color(x, y, ndvi_pixel)
       end
-      if x == (val + 200)
-        puts "Columns so far: " + x.to_s
-        puts "Time: " + Time.now.to_s
-        val = val + 200
-      end
-
     end
 
-  	lNDVIImage.write(lDestination)
-    puts "NDVI Complete!"
-    puts Time.now
-    #TODO: Save Image
-    #TODO: Delete Red NIR
-    #TODO: Set New Image Path to Item
-    #TODO: Notify Area Update that it is complete
+  	ndvi_image.write(destination)
+    File.delete(red_path)
+    File.delete(nir_path)
+    area_update.image_archive_url = destination
   end
 
   #Returns the NDVI to Color map result for each pixel's NDVI
-  def color_result(aValue)
-     
-    if aValue <=-1.000
-      return Pixel.new(  5,  24,  82) 
-    elsif aValue <=-0.300 
-      return Pixel.new(  5,  24,  82)
-    elsif aValue <=-0.180 
-      return Pixel.new(255, 255, 255)
-    elsif aValue <= 0.000 
-      return Pixel.new(255, 255, 255)
-    elsif aValue <= 0.025 
-      return Pixel.new(206, 197, 180)
-    elsif aValue <= 0.075 
-      return Pixel.new(191, 163, 124)
-    elsif aValue <= 0.125 
-      return Pixel.new(179, 174,  96)
-    elsif aValue <= 0.150 
-      return Pixel.new(163, 181,  80)
-    elsif aValue <= 0.175 
-      return Pixel.new(144, 170,  60)
-    elsif aValue <= 0.233 
-      return Pixel.new(166, 195,  29)
-    elsif aValue <= 0.266 
-      return Pixel.new(135, 183,   3)
-    elsif aValue <= 0.333 
-      return Pixel.new(121, 175,   1)
-    elsif aValue <= 0.366 
-      return Pixel.new(101, 163,   0)
-    elsif aValue <= 0.433 
-      return Pixel.new( 78, 151,   0)
-    elsif aValue <= 0.466 
-      return Pixel.new( 43, 132,   4)
-    elsif aValue <= 0.550 
-      return Pixel.new(  0, 114,   0)
-    elsif aValue <= 0.650 
-      return Pixel.new(  0,  90,   1)
-    elsif aValue <= 0.750 
-      return Pixel.new(  0,  73,   0)
-    elsif aValue <= 0.850 
-      return Pixel.new(  0,  56,   0)
-    elsif aValue <= 0.950 
-      return Pixel.new(  0,  31,   0)
-    elsif aValue <= 1.000 
-      return Pixel.new(  0,   0,   0)
-    else                  
-      return Pixel.new(255, 255, 255) 
-    end
+  def color_result(pixel_value)
+    return Pixel.new(  5,  24,  82) if pixel_value <=-1.000
+    return Pixel.new(  5,  24,  82) if pixel_value <=-0.300 
+    return Pixel.new(255, 255, 255) if pixel_value <=-0.180 
+    return Pixel.new(255, 255, 255) if pixel_value <= 0.000
+    return Pixel.new(206, 197, 180) if pixel_value <= 0.025
+    return Pixel.new(191, 163, 124) if pixel_value <= 0.075
+    return Pixel.new(179, 174,  96) if pixel_value <= 0.125
+    return Pixel.new(163, 181,  80) if pixel_value <= 0.150
+    return Pixel.new(144, 170,  60) if pixel_value <= 0.175
+    return Pixel.new(166, 195,  29) if pixel_value <= 0.233
+    return Pixel.new(135, 183,   3) if pixel_value <= 0.266
+    return Pixel.new(121, 175,   1) if pixel_value <= 0.333
+    return Pixel.new(101, 163,   0) if pixel_value <= 0.366
+    return Pixel.new( 78, 151,   0) if pixel_value <= 0.433
+    return Pixel.new( 43, 132,   4) if pixel_value <= 0.466
+    return Pixel.new(  0, 114,   0) if pixel_value <= 0.550
+    return Pixel.new(  0,  90,   1) if pixel_value <= 0.650
+    return Pixel.new(  0,  73,   0) if pixel_value <= 0.750
+    return Pixel.new(  0,  56,   0) if pixel_value <= 0.850
+    return Pixel.new(  0,  31,   0) if pixel_value <= 0.950
+    return Pixel.new(  0,   0,   0) if pixel_value <= 1.000
+    return Pixel.new(255, 255, 255)
   end
 
-  def calculate_ndvi(aNIR, aRed)
-    lNumerator = aNIR - aRed
-    if lNumerator == 0
-      return 0
-    end
+  def calculate_ndvi(nir, red)
+    numerator = nir - red
+    return 0 if numerator == 0
 
-    lDemoninator = aNIR + aRed
-    if lDemoninator == 0 
-      return 0
-    end
+    demoninator = nir + red
+    return 0 if demoninator == 0 
 
-    return lNumerator.to_f / lDemoninator
+    return numerator.to_f / demoninator
   end
 end
-
-#B3 = "/home/megurineluka/BunjilForestWatch/Codebase/bunjil/app/jobs/B3.TIF"
-#B4 = "/home/megurineluka/BunjilForestWatch/Codebase/bunjil/app/jobs/B4.TIF"
-#x = ImageProcessorJob.new()
-#x.perform(0, B3, B4)
-#puts "done"
