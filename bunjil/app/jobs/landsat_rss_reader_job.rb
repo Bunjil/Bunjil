59481852c7b8f4f756ae5e6e70d494d27790cc20
DavidWillie <6955193@student.swin.edu.au>
2012-10-05 00:52:08 +0000

    DW - N/A - Code Review: Cleaned up the the RSS reader. Spaced operators, cleaned up spacing and comments. Aligned large assignment blocks.
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@231 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/app/jobs/landsat_rss_reader_job.rb
+++ bunjil/app/jobs/landsat_rss_reader_job.rb
@@ -5,28 +5,34 @@ require 'feedzirra'
 require 'nokogiri'
 
 class LandsatRssReaderJob
-  def perform (limit=-1, autoDL=false)
+
+  def perform (limit =- 1, autoDL = false)
     feed = Feed.find_by_name("LandSat7")
     ActiveRecord::Base.logger.debug "Fetching Feed... #{feed.name}"
-    area_updates=[]
+    area_updates = []
     rss_data = Feedzirra::Feed.fetch_and_parse(feed.url)
+    
     # Note for some reason all tag names are different.
-    # If the feed fails to be parses/fetched, just give up.
+    # If the feed fails to be parsed/fetched, just give up.
     return if rss_data.is_a?(Fixnum) 
 
     rss_data.entries.each_with_index do |item,ind|
-      a=nil
+      a = nil
+      
       # Only new entries: ignore entries with duplicate scene id.
       ActiveRecord::Base.logger.debug 'Reading an RSS feed item...'
-      scene_id=item.summary.scan(/Scene ID: (\w*)/)[0][0]
+      
+      scene_id = item.summary.scan(/Scene ID: (\w*)/)[0][0]
       if FeedItem.find_by_scene_id scene_id
         ActiveRecord::Base.logger.debug "This Item already exists in the DB (scene id: #{scene_id})"
       else
         a=handle_new_item(item, feed.id) end 
+      
       # only check ones that get saved.
       area_updates.push(a) unless a==nil
-      break if ind==limit
+      break if ind == limit
     end
+
     ActiveRecord::Base.logger.debug "Calling intersection checking job with #{area_updates.count} new area updates."
     IntersectionCheckingJob.perform area_updates, autoDL
     area_updates
@@ -39,9 +45,11 @@ class LandsatRssReaderJob
     feed_item.feed_id  = feed_id
     feed_item.link     = item.url
     ActiveRecord::Base.logger.debug "This item is new to the database, id: #{feed_item.scene_id}"
+    
     if feed_item.save
       # Create area update with url and feed lat/long points.
       area_update = AreaUpdate.new
+      
       # return it if it's valid and requires intersection check.
       attrs=init_area_update(feed_item, item.summary)
       if area_update.init(attrs)
@@ -51,34 +59,36 @@ class LandsatRssReaderJob
         ActiveRecord::Base.logger.debug "Rejected due to cloud cover. (It is #{attrs[:cloud_cover]})"
       end
     end
+    
     return nil
   end
 
-  def init_area_update item, desc
-    doc=Nokogiri::HTML(open(item.get_formatted_url))
+  def init_area_update(item, desc)
+    doc = Nokogiri::HTML(open(item.get_formatted_url))
     # note, these do change.
-    attrs={}
-    attrs[:tl_lat]=search doc, 'Corner UL Latitude Product'
-    attrs[:tr_lat]=search doc, 'Corner UR Latitude Product'
-    attrs[:br_lat]=search doc, 'Corner LR Latitude Product'
-    attrs[:bl_lat]=search doc, 'Corner LL Latitude Product'
-    attrs[:tl_lon]=search doc, 'Corner UL Longitude Product'
-    attrs[:tr_lon]=search doc, 'Corner UR Longitude Product'
-    attrs[:br_lon]=search doc, 'Corner LR Longitude Product'
-    attrs[:bl_lon]=search doc, 'Corner LL Longitude Product'
-    attrs[:cloud_cover]=search doc, 'Cloud Cover'
-    attrs[:feed_item_id]=item.id
-    attrs[:image_url]=format_image_url desc
+    attrs = {}
+    attrs[:tl_lat]       = search doc, 'Corner UL Latitude Product'
+    attrs[:tr_lat]       = search doc, 'Corner UR Latitude Product'
+    attrs[:br_lat]       = search doc, 'Corner LR Latitude Product'
+    attrs[:bl_lat]       = search doc, 'Corner LL Latitude Product'
+    attrs[:tl_lon]       = search doc, 'Corner UL Longitude Product'
+    attrs[:tr_lon]       = search doc, 'Corner UR Longitude Product'
+    attrs[:br_lon]       = search doc, 'Corner LR Longitude Product'
+    attrs[:bl_lon]       = search doc, 'Corner LL Longitude Product'
+    attrs[:cloud_cover]  = search doc, 'Cloud Cover'
+    attrs[:feed_item_id] = item.id
+    attrs[:image_url]    = format_image_url desc
     attrs
   end
   # uses item.description to get the image url
-  def format_image_url raw
-    doc=Nokogiri::HTML(raw)
+  def format_image_url(raw)
+    doc = Nokogiri::HTML(raw)
     doc.xpath('//a[1]/@href').text
   end
+  
   # search the lansat specific image page for some text in the table and
-    # return it's value.
-  def search(doc,search)
+  # return it's value.
+  def search(doc, search)
     basePath = '/html/body/div/table/tbody/tr/td/a[text() ="'
     doc.xpath(basePath+search+'"]/../../td[2]').text
   end
