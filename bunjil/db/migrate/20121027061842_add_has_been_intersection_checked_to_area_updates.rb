004a25a98aee8118b01806fa97db316b8849a56b
KarlGlaser <6955002@student.swin.edu.au>
2012-10-27 06:44:07 +0000

    KG - 38529431 -has_been_intersection_checked also needed a default.
    git-svn-id: svn+ssh://mercury.it.swin.edu.au/home/students/accounts/s6955436/hit3158_01/repos/Codebase@289 cefe45ee-12bb-0410-9336-bd77ef291008

--- bunjil/db/migrate/20121027061842_add_has_been_intersection_checked_to_area_updates.rb
+++ bunjil/db/migrate/20121027061842_add_has_been_intersection_checked_to_area_updates.rb
@@ -1,5 +1,5 @@
 class AddHasBeenIntersectionCheckedToAreaUpdates < ActiveRecord::Migration
   def change
-    add_column "area_updates", :has_been_intersection_checked, :bool
+    add_column "area_updates", :has_been_intersection_checked, :boolean, :default => false, :null => false
   end
 end
